21e856aa5cb94e4eb8c4a60a06e1482c2dffcfaf
Lizeth <lissur777@gmail.com>
2024-06-20 21:10:34 -0600

    modifying the renderer and adding comments

--- renderer.py
+++ renderer.py
@@ -1,8 +1,10 @@
+#import classes and modules necessary for rendering
 from shader_program import ShaderProgram
 from camera import Camera
 from meshes.point_cloud_mesh import PointCloudMesh
 from meshes.axis_mesh import AxisMesh
 
+#The class is responsible for managing the configuration and rendering for its visual elements
 
 class Renderer:
     def __init__(self, app):
@@ -19,7 +21,8 @@ class Renderer:
             scale=self.app.data_loader.scale,
             center=self.app.data_loader.center
         )
-
+    #The update() and render() methods are responsible for updating the components and rendering the objects
+    
     def update(self):
         self.camera.update()
         self.shader_program.update()
